eb1eae0a74cf33507b23c5453e61ada2adfd7b14
Mohd zahid <62196698+mdzahid01@users.noreply.github.com>
2023-08-28 15:51:33 +0530

    Update script.js
    fixed navbar toggle problem

--- script.js
+++ script.js
@@ -10,11 +10,10 @@ menuToggle.onclick = function(){
 
 // यह लाइन डॉक्यूमेंट पर क्लिक के लिए एक ईवेंट लिस्टेनर जोड़ती है। जब किसी क्लिक की जाती है, तो यह ईवेंट तब होता है।
 document.addEventListener('click', (event) => {
-    // यह नया वेरिएबल target बनाता है और उसमें वह एलिमेंट स्टोर करता है जिस पर क्लिक हुआ है।
     const target = event.target;
 
-    // यह कंडीशन जांचता है कि क्या क्लिक का टारगेट मेनू और मेनू आइकन के बाहर है और क्या मेनू खुला हुआ है (toggle टॉगल हुआ है)।
-    if (!navbar.contains(target) && !header.contains(target)) {
-        header.classList.toggle('active');
+    // Check if the target is not the menuToggle and the header, and the header is currently active.
+    if (!navbar.contains(target) && !header.contains(target) && header.classList.contains('active')) {
+        header.classList.remove('active');
     }
 });
